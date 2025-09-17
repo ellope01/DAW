77ee9de92b5e487b7100d20f76466fb21a90c23a
ellope01 <algieba23@gmail.com>
2025-09-17 17:41:43 +0200

    Ejercicio1Bucles

---
+++ DAW_Client/JavaScript/ObjetosyClases/Ejercicio1.js
@@ -0,0 +1,17 @@
+//Ejercicio 1
+const intervaloNumerico = {
+    numeroMinimo : window.prompt("Introduce el número por el que empieza el intervalo: "),
+    numeroMaximo: window.prompt("Introduce el número por donde acaba el intervalo"),
+
+    get ncomprendidos(){
+        let arrayNumeros;
+        do{
+            numero = `${this.numeroMinimo}`;
+            arrayNumeros[numero++]
+        }
+        while(numero <= `${this.numeroMaximo}`);
+        return arrayNumeros
+    }
+};
+console.log(intervaloNumerico.ncomprendidos)
+
