c6b19a52728e12dd897205c6ff5fb44086700ff4
ellope01 <algieba23@gmail.com>
2025-10-05 16:39:06 +0200

    reloj comentado

--- DAW_Client/JavaScript/Reloj/reloj.js
+++ DAW_Client/JavaScript/Reloj/reloj.js
@@ -1,5 +1,9 @@
 function mostrarReloj() {
+    //seleccionamos los elementos que queremos cambiar, en este caso la hora y la fecha
     const hora = document.querySelector('#hora');
+    const fecha = document.getElementById('fecha');
+
+    //creamos variables para asignar las opciones de la hora y de la fecha
     let opcionesHora = {
         hour: '2-digit',
         minute: '2-digit',
@@ -13,17 +17,17 @@ function mostrarReloj() {
         month: 'short'
     };
 
-    const fecha = document.getElementById('fecha');
-
+    //creamos un Date
     let date = new Date();
 
+    //configuramos lo que queremos que se muestre en la fecha y la hora
     dateFecha = date.toLocaleDateString('es-ES', { ...opcionesFecha, timeZone: 'Europe/Madrid' });
-
     dateHora = date.toLocaleTimeString('es-ES', opcionesHora);
 
-
+    //mostramos los elementos cambiados
     fecha.textContent = dateFecha;
     hora.textContent = dateHora;
 
+    //utilizamos un toggle para que se cambie como un intermitente el borde del reloj
     hora.parentElement.classList.toggle('animar');
 }
