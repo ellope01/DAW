68707659b35baf36868eb1d3fd5cb14ec04569fd
ellope01 <algieba23@gmail.com>
2025-10-05 17:01:11 +0200

    barra porcentajes hecha y comentada

--- DAW_Client/JavaScript/Barra Progreso/index.js
+++ DAW_Client/JavaScript/Barra Progreso/index.js
@@ -1,10 +1,19 @@
-function barraPorcentaje(){
+function barraPorcentaje() {
+    //seleccionamos los elementos que queremos modificar
     const porcentaje = document.querySelector('.contenedor .contador');
-    console.log(porcentaje);
+    const barraColor = document.querySelector('.contenedor .barraFront')
+    let valor = 0
 
-    for(let i=0; i<100;i++){
-        porcentaje.textContent = porcentaje
-        porcentaje ++;
-    }
+    //creamos un intervalo que se ejecuta cada 800 segundos
+    const intervalo = setInterval(() => {
+        valor++;
+        //actualizamos el contenido de porcentaje y de la barra de color
+        porcentaje.textContent = valor + '%'
+        barraColor.style.width = valor + "%";
 
+        //cuando el valor(contador) llega a 100 deja de contar
+        if (valor >= 100) {
+            clearInterval(intervalo);
+        }
+    }, 800);
 }
