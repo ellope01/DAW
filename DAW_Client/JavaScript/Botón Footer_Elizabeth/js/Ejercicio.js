979d0c6feedef7a2246d52f496358bc2bb4be238
ellope01 <algieba23@gmail.com>
2025-10-06 16:33:29 +0200

    subiendo archivos

---
+++ DAW_Client/JavaScript/Botón Footer_Elizabeth/js/Ejercicio.js
@@ -0,0 +1,10 @@
+function clickarBoton() {
+    let boton = document.querySelector('.btn-flotante');
+    if(boton){
+        boton.style.backgroundColor = "red"
+        boton;
+    }else{
+        boton.style.backgroundColor = "white"
+    }
+}
+
