a2e30b3f130d2935474b83c534f769d2a8e61094
ellope01 <algieba23@gmail.com>
2025-10-08 15:11:04 +0200

    ejercicioCarrito

---
+++ DAW_Client/JavaScript/Carrito Compra_Elizabeth/Ejercicio.js
@@ -0,0 +1,15 @@
+//utilizamos un querySelector y querySelectorAll para seleccionar los elementos que interesan
+let agragarCarrito = document.querySelectorAll();
+let seleccionarCarrito = document.querySelector('#carrito');
+let eliminarCurso = document.querySelector();
+let vaciarCarrito = document.querySelector('#vaciar-carrito'); 
+
+let infoCurso = [];
+
+infoCurso = {
+    imagen : "imagen",
+    titulo : "titulo",
+    precio : "precio",
+    id : "id",
+    cantidad : "cantidad"
+}
