4888c297a0a2f6a7fbf65e33ff3c850a82858cbe
ellope01 <algieba23@gmail.com>
2025-10-08 17:35:27 +0200

    modificando ejericio carrito

--- DAW_Client/JavaScript/Carrito Compra_Elizabeth/Ejercicio.js
+++ DAW_Client/JavaScript/Carrito Compra_Elizabeth/Ejercicio.js
@@ -1,6 +1,6 @@
 let carrito = document.querySelector('#carrito');
 let eliminarCurso = document.querySelector();
-let vaciarCarrito = document.querySelector('#vaciar-carrito'); 
+let btnVaciarCarrito = document.querySelector('#vaciar-carrito');
 let cursoSeleccionado = document.querySelector();
 
 const btnsAgregarCarrito = document.querySelectorAll('.miBoton')
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded');
 
 
 
-btnsAgregarCarrito.forEach(function(btnsAgregarCarrito) {
-    btnAgragarCarrito.addEventListener('click', function() {
+btnsAgregarCarrito.forEach(function (btnsAgregarCarrito) {
+    btnAgragarCarrito.addEventListener('click', function () {
         console.log('Botón clickeado:', btnsAgregarCarrito.textContent);
     });
 });
@@ -19,21 +19,21 @@ btnsAgregarCarrito.forEach(function(btnsAgregarCarrito) {
 
 const infoCurso = [];
 infoCurso = {
-    imagen :cursoSeleccionado.querySelector("img").src,
-    titulo : cursoSeleccionado.querySelector("h4").textContent,
+    imagen: cursoSeleccionado.querySelector("img").src,
+    titulo: cursoSeleccionado.querySelector("h4").textContent,
     precio: cursoSeleccionado.querySelector('.precio span').textContent,
     id: cursoSeleccionado.querySelector('a').getAttribute('data-id'),
-    cantidad : 1
+    cantidad: 1
 }
 
 let listaCarrito = [];
 
-if(listaCarrito){
+if (listaCarrito) {
     //si el curso se comprado se agrega
     let cursoClicado = curso
 }
 
-btnAgragarCarrito.addEventListener("click",() =>{
+btnAgragarCarrito.addEventListener("click", () => {
     const titulo = document.createElement(infoCurso.titulo);
     const precio = document.createElement(infoCurso.precio);
     const imagen = document.createElement(infoCurso.imagen);
@@ -44,16 +44,35 @@ btnAgragarCarrito.addEventListener("click",() =>{
 }
 )
 
+//tr fila
+//td celda
+//Ejem. createElement('tr');
 const existe = carrito.some(curso = curso.cursoSeleccionado === infoCurso.id)
-if(carrito !== null){
+if (carrito !== null) {
     //cuando el carrito tiene elementos
-    if(existe){
+    if (existe) {
         let cont = btnsAgregarCarrito.infoCurso.contador;
-        cont ++;
+        cont++;
         let contCambiado = contador.textContent = cont;
         carrito.appendChild(carrito.infoCurso.contador(contCambiado))
 
-    }else{
+    } else {
         let curso = carrito.push;
     }
+}
+
+//Funcion Borrar Curso del carrito que cuando se clicke sobre la x se borrara el curso del carrito
+function borrarCurso() {
+    btnBorrarCurso.addEventListener("click", () => {
+
+    })
+}
+
+
+//Funcion vaciar Carrito que cuando los clickes se borra el carrito entero
+function vaciarCarrito() {
+    btnVaciarCarrito.addEventListener("click", () => {
+        let nuevoCarrito = [];
+        listaCarrito = nuevoCarrito;
+    })
 }
