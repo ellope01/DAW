1f1083bb398096d73df246b07ac7183a07cd3f1a
ellope01 <algieba23@gmail.com>
2025-10-08 16:25:33 +0200

    agregarndo seleccionar boton y añadiendo informacion

--- DAW_Client/JavaScript/Carrito Compra_Elizabeth/Ejercicio.js
+++ DAW_Client/JavaScript/Carrito Compra_Elizabeth/Ejercicio.js
@@ -1,15 +1,43 @@
-//utilizamos un querySelector y querySelectorAll para seleccionar los elementos que interesan
-let agragarCarrito = document.querySelectorAll();
-let seleccionarCarrito = document.querySelector('#carrito');
+let carrito = document.querySelector('#carrito');
 let eliminarCurso = document.querySelector();
 let vaciarCarrito = document.querySelector('#vaciar-carrito'); 
+let cursoSeleccionado = document.querySelector();
 
-let infoCurso = [];
+const btnsAgregarCarrito = document.querySelectorAll('.miBoton')
 
+btnsAgregarCarrito.forEach(function(btnsAgregarCarrito) {
+    btnAgragarCarrito.addEventListener('click', function() {
+        console.log('Botón clickeado:', btnsAgregarCarrito.textContent);
+    });
+});
+
+
+const infoCurso = [];
 infoCurso = {
-    imagen : "imagen",
-    titulo : "titulo",
-    precio : "precio",
-    id : "id",
-    cantidad : "cantidad"
+    imagen :cursoSeleccionado.querySelector("img").src,
+    titulo : cursoSeleccionado.querySelector("h4").textContent,
+    precio: cursoSeleccionado.querySelector('.precio span').textContent,
+    id: cursoSeleccionado.querySelector('a').getAttribute('data-id'),
+    cantidad : 1
+}
+
+let listaCarrito = [];
+
+if(listaCarrito){
+    //si el curso se comprado se agrega
+    let cursoClicado = curso
+}
+
+btnAgragarCarrito.addEventListener("click",() =>{
+    const titulo = document.createElement(infoCurso.titulo);
+    const precio = document.createElement(infoCurso.precio);
+    carrito.appendChild(titulo)
+    carrito.appendChild(precio)
+}
+)
+let cursoExiste;
+if(cursoExiste){
+    //tenemos que añadir uno a la cantidad
+}else{
+    //agregamos uno nuevo
 }
