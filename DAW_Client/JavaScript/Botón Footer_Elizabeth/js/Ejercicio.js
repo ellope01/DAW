68778def9c8f3256f82a911ce1cd8e659706e584
ellope01 <algieba23@gmail.com>
2025-10-11 02:53:51 +0200

    footer acabado faltan comentarios

--- DAW_Client/JavaScript/Botón Footer_Elizabeth/js/Ejercicio.js
+++ DAW_Client/JavaScript/Botón Footer_Elizabeth/js/Ejercicio.js
@@ -1,10 +1,14 @@
 function clickarBoton() {
     let boton = document.querySelector('.btn-flotante');
-    if(boton){
-        boton.style.backgroundColor = "red"
-        boton;
-    }else{
-        boton.style.backgroundColor = "white"
-    }
+    let footer = document.querySelector('.footer');
+
+    boton.addEventListener("click", () => {
+        footer.classList.toggle('activo');
+        if (footer.classList.contains('activo')) {
+            boton.style.backgroundColor = "red";
+        } else {
+            boton.style.backgroundColor = "white";
+        }
+    });
 }
 
