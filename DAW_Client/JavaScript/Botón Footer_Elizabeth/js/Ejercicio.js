c0c124346aadb665472ca42382d3e2897c5098b3
ellope01 <algieba23@gmail.com>
2025-10-14 15:53:51 +0200

    subido todo

--- DAW_Client/JavaScript/Botón Footer_Elizabeth/js/Ejercicio.js
+++ DAW_Client/JavaScript/Botón Footer_Elizabeth/js/Ejercicio.js
@@ -1,7 +1,12 @@
+//funcion para el boton y footer
 function clickarBoton() {
+    //seleccionamos el boton y el footer del documento
     let boton = document.querySelector('.btn-flotante');
     let footer = document.querySelector('.footer');
 
+    //añadimos un evento que al hacer click
+    //activamos y desactivamos el footer 
+    //y cambiamos el color de rojo a blanco
     boton.addEventListener("click", () => {
         footer.classList.toggle('activo');
         if (footer.classList.contains('activo')) {
