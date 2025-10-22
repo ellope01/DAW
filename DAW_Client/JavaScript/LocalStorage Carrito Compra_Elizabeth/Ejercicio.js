69e53dd235949eb8fbe698a6a726dbd437fbd04e
ellope01 <algieba23@gmail.com>
2025-10-22 16:22:01 +0200

    ejercicio 7 acabado y comentado

--- DAW_Client/JavaScript/LocalStorage Carrito Compra_Elizabeth/Ejercicio.js
+++ DAW_Client/JavaScript/LocalStorage Carrito Compra_Elizabeth/Ejercicio.js
@@ -3,7 +3,8 @@ const carritoTbody = document.querySelector('#carrito tbody'); // donde mostrare
 const listaCursos = document.querySelector('#lista-cursos');   // donde están los productos
 const btnVaciarCarrito = document.querySelector('#vaciar-carrito'); // botón para vaciar el carrito
 
-let articulosCarrito = [];
+//Iniciamos el carrito con lo que hay en el localStorage
+let articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 // Función para pintar el carrito, esta recorre el array articulosCarritos
 // y crea las filas usando Template Strings
@@ -24,13 +25,14 @@ function pintarCarrito() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM cargado - listeners listos.");
 
+    //ponemos aqui la funcion pintarCarrito para que lo primero que haga sea volver a imprimir la lista de cursos
+    pintarCarrito();
     // Para añadir cursos al carrito
-    listaCursos.addEventListener('click', function(e) {
-        if(e.target.classList.contains('agregar-carrito')) {
+    listaCursos.addEventListener('click', function (e) {
+        if (e.target.classList.contains('agregar-carrito')) {
             e.preventDefault();
-            
+
             //Aceder al curso y recoger los datos
             const cursoElem = e.target.parentElement.parentElement;
             const infoCurso = {
@@ -54,25 +56,34 @@ document.addEventListener('DOMContentLoaded', function () {
                 //si no, al array
                 articulosCarrito.push(infoCurso);
             }
+            //cada vez que se añada un curso lo guardamos en el localStorage
+            localStorage.setItem("carrito", JSON.stringify(articulosCarrito));
 
             pintarCarrito();
         }
     });
 
     // Para borrar un curso del carrito
-    carritoTbody.addEventListener('click', function(e) {
-        if(e.target.classList.contains('borrar-curso')) {
+    carritoTbody.addEventListener('click', function (e) {
+        if (e.target.classList.contains('borrar-curso')) {
             const idCurso = e.target.getAttribute('data-id');
             //filtramos para eliminar po id
             articulosCarrito = articulosCarrito.filter(c => c.id !== idCurso);
+            //actualizamos el localStorage con el nuevo carrito (con un articulo eliminado)
+            localStorage.setItem("carrito",JSON.stringify(articulosCarrito));
             pintarCarrito();
         }
     });
 
     // Para vaciar el carrito
-    btnVaciarCarrito.addEventListener('click', function(e) {
+    btnVaciarCarrito.addEventListener('click', function (e) {
         e.preventDefault();
         articulosCarrito = [];
+        //ponemos aqui el localStorage.clear() para eliminar el localStorage entero
+        localStorage.clear();
         pintarCarrito();
     });
+
 });
+
+
