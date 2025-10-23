da75adaddcea01641bcc38aa41b3c901cb961f5f
ellope01 <algieba23@gmail.com>
2025-10-23 17:49:29 +0200

    añadiendo errores ejercicio 8

--- DAW_Client/JavaScript/LocalStorageTODO_Elizabeth/ejercicio.js
+++ DAW_Client/JavaScript/LocalStorageTODO_Elizabeth/ejercicio.js
@@ -1,13 +1,35 @@
 const formulario = document.querySelector('#formulario');
 const btnAgregar = formulario.querySelector('input[type=submit]');
 
-const texto=32;
-let error;
+const listaTareas = [];
 
-if(texto>30){
-    error = "LA TAREA ES DEMASIADO LARGA ...";
-}else if(texto === ""){
-    error = "NO HAS PUESTO NINGUNA TAREA ...";
-}else{
-    error= "LA TAREA YA EXISTE";
+function validarTareas() {
+    const tarea = formulario.querySelector('#tarea').value;
+    if (tarea.length >= 31) {
+        return "La tarea es demasiado larga ...";
+    } else if (tarea === "") {
+        return "No has puesto ninguna tarea ...";
+    }
+    return null;
 }
+
+function mostrarMensajeError(mensaje) {
+    if (mensaje !== null) {
+        const divMensj = document.createElement('div');
+        divMensj.classList.add('error');
+
+        divMensj.textContent = mensaje;
+        document.querySelector('#contenido').appendChild(divMensj);
+        setTimeout(() => divMensj.remove(), 1500);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    formulario.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const mensaje = validarTareas();
+        mostrarMensajeError(mensaje);
+        
+    })
+});
