b3857eabe4216263929001296931ec934b3d7c89
ellope01 <algieba23@gmail.com>
2025-11-04 17:14:52 +0100

    cambios de la imagen

--- DAW_Server/discografia/imagen.php
+++ DAW_Server/discografia/imagen.php
@@ -5,20 +5,24 @@
         $finfo -> file($_FILES['upfile']['tmp_name']),
         array(
             'jpg' => 'image/jpeg',
-            'png' => 'image/png'
+            'png' => 'image/png',
         ),
         true
     )){
         throw new RuntimeException('Invalid file format. ');
     }
 
+    //Cargamos la imagen
+    if($ext === 'png'){
+        $image = imagecreatefrompng($_FILES['upfile']['tmp_name']);
+    }else{
+        $image = imagecreatefromjpeg($_FILES['upfile']['tmp_name']);
+    }
 
-    //Escalar la imagen 360x480
-    imagescale(
-        $image,
-        360,
-        480
-    ); 
+    //Conseguimos los datos de la imagen
+    $tamano = getimagesize($_FILES['upfile']['tmp_name']);
+    $ancho = $tamano[0];
+    $alto = $tamano[1];
 
     //Miramos si la imagen es valida en cuanto al tamaño
     if ($ancho > 360 || $alto > 480 ){
@@ -26,6 +30,11 @@
             "La imagen es demasiado grande, el máximo permitivo es 360x480px"
         );
     }else{
-        
+        $profileImage = imagescale(
+            $image, 360, 480
+        );
+        $loggedImage = imagescale(
+            $image, 72, 96
+        );
     }
 ?>
