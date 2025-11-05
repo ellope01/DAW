b721c313a31c70a1bd9dc228eadc0d9be2714a48
ellope01 <algieba23@gmail.com>
2025-11-05 15:57:10 +0100

    empezando ejercicio

--- DAW_Client/JavaScript/Ejercicio CRUD/js/API.js
+++ DAW_Client/JavaScript/Ejercicio CRUD/js/API.js
@@ -1,1 +1,17 @@
 //Acciones con la API de Json Server
+async function agregarCliente() {
+    try {
+
+        const respuesta = await fetch(url,{
+            method : 'POST',
+            body: JSON.stringify(cliente),
+            headers:{
+                'Content-type' : 'application/json'
+            }
+        });
+
+        return respuesta;
+    } catch (error) {
+        console.log('Error al agragar cliente');
+    }
+}
--- DAW_Client/JavaScript/Ejercicio CRUD/js/nuevocliente.js
+++ DAW_Client/JavaScript/Ejercicio CRUD/js/nuevocliente.js
@@ -1,1 +1,19 @@
 //todo lo relacionado con la validación y llamada a función conexión API
+const formulario = document.querySelector('#formulario');
+const nombreCliente = formulario.querySelector('#nombre');
+const correoCliente = formulario.querySelector('#email');
+const telefonoCliente = formulario.querySelector('#telefono');
+const empresaCliente = formulario.querySelector('#empresa');
+const agragarCliente = formulario.querySelector('input[type=submit]');
+
+function comprobarCampos() {
+
+};
+
+function mensajeError() {
+
+};
+
+agragarCliente.addEventListener('submit', e => {
+    
+})
