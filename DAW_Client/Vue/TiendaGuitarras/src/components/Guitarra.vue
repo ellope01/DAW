243ccad1aeb3bbad00a92d753391ce569b96bc06
ellope01 <algieba23@gmail.com>
2025-12-02 12:17:01 +0100

    vue natalia

--- DAW_Client/Vue/TiendaGuitarras/src/components/Guitarra.vue
+++ DAW_Client/Vue/TiendaGuitarras/src/components/Guitarra.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="col-md-6 col-lg-4 my-4 row align-items-center">
     <div class="col-4">
-      <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra" />
+      <img
+        class="img-fluid"
+        v-bind:src="`./img/${guitarra.imagen}.jpg`"
+        v-bind:alt="'imagen guitarra' + guitarra.nombre"
+      />
     </div>
     <div class="col-8">
       <h3 class="text-black fs-4 fw-bold text-uppercase">
-        {{guitarra.nombre}}
+        {{ guitarra.nombre }}
       </h3>
-      <p>{{guitarra.descripcion}}</p>
-      <p class="fw-black text-primary fs-3">{{guitarra.precio}}$</p>
-      <button type="button" class="btn btn-dark w-100">
+      <p>{{ guitarra.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">{{ guitarra.precio }}$</p>
+      <p>{{ numero }}</p>
+      <button type="button" class="btn btn-dark w-100" @:click="$emit('agregar-carrito',guitarra)">
         Agregar al Carrito
       </button>
     </div>
@@ -18,12 +23,16 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["incrementar","agregar-carrito"]);
+
 const props = defineProps({
-    guitarra:{
-        type:Object,
-        required:true
-    }
-})
+  guitarra: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
 
 <style lang="scss" scoped></style>
