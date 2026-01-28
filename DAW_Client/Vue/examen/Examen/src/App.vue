a7b3d21291d05ec589e81900e05c252772b9b4f0
ellope01 <algieba23@gmail.com>
2026-01-28 17:53:40 +0100

    localStorage

--- DAW_Client/Vue/examen/Examen/src/App.vue
+++ DAW_Client/Vue/examen/Examen/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { reactive, ref, onMounted, watch } from "vue";
 import Formulario from "./components/Formulario.vue";
 import { generarID } from "./helpers";
 
@@ -16,13 +16,16 @@ const cita = reactive({
 
 const guardarCita = () => {
   if (cita.id) {
+    
     const index = citas.value.findIndex((c) => c.id === cita.id);
     citas.value[index] = { ...cita };
   } else {
+    
     cita.id = generarID();
     citas.value.push({ ...cita });
   }
 
+  
   Object.assign(cita, {
     nombreMascota: "",
     nombrePropietario: "",
@@ -33,13 +36,31 @@ const guardarCita = () => {
   });
 };
 
-
-const borrarCita = (email) => {
-  const index = citas.value.findIndex((c) => c.email === email);
-  if (index !== -1) {
-    citas.value.splice(index, 1);
-  }
+const borrarCita = (id) => {
+  citas.value = citas.value.filter((c) => c.id !== id);
 };
+
+
+const guardarLocalStorage = () => {
+  localStorage.setItem("citas", JSON.stringify(citas.value));
+};
+
+
+watch(
+  citas,
+  () => {
+    guardarLocalStorage();
+  },
+  { deep: true }
+);
+
+
+onMounted(() => {
+  const citasLS = localStorage.getItem("citas");
+  if (citasLS) {
+    citas.value = JSON.parse(citasLS);
+  }
+});
 </script>
 
 <template>
@@ -61,6 +82,7 @@ const borrarCita = (email) => {
           <span class="text-indigo-600 font-bold">Citas de Pacientes</span>
         </p>
       </div>
+
       <div v-if="citas.length === 0" class="mt-20 text-2xl text-center">
         No hay Pacientes
       </div>
@@ -70,35 +92,46 @@ const borrarCita = (email) => {
         :key="citaItem.id"
         class="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl"
       >
-        <p class="font-bold mb-3 text-gray-700 uppercase">ID:
-          <span class="font-normal normal-case"
-            >{{ citaItem.nombreMascota }}</span
-          >
-        </p>
-        <p class="font-bold mb-3 text-gray-700 uppercase">Mascota:
-          <span class="font-normal normal-case"
-            >{{ citaItem.nombreMascota }}</span
-          >
-        </p>
-        <p class="font-bold mb-3 text-gray-700 uppercase">Propietario:
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          ID:
           <span class="font-normal normal-case">
-            {{ citaItem.nombrePropietario }}</span
-          >
+            {{ citaItem.id }}
+          </span>
         </p>
-        <p class="font-bold mb-3 text-gray-700 uppercase">Email:
+
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          Mascota:
           <span class="font-normal normal-case">
-            {{ citaItem.email }}</span
-          >
+            {{ citaItem.nombreMascota }}
+          </span>
         </p>
-        <p class="font-bold mb-3 text-gray-700 uppercase">Fecha:
+
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          Propietario:
           <span class="font-normal normal-case">
-            {{ citaItem.fecha }}</span
-          >
+            {{ citaItem.nombrePropietario }}
+          </span>
         </p>
-        <p class="font-bold mb-3 text-gray-700 uppercase">Sintomas:
+
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          Email:
           <span class="font-normal normal-case">
-            {{ citaItem.sintomas }}</span
-          >
+            {{ citaItem.email }}
+          </span>
+        </p>
+
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          Fecha:
+          <span class="font-normal normal-case">
+            {{ citaItem.cita }}
+          </span>
+        </p>
+
+        <p class="font-bold mb-3 text-gray-700 uppercase">
+          Síntomas:
+          <span class="font-normal normal-case">
+            {{ citaItem.sintomas }}
+          </span>
         </p>
 
         <div class="grid md:grid-cols-2 gap-5 mt-10">
@@ -108,11 +141,11 @@ const borrarCita = (email) => {
           >
             Editar
           </button>
+
           <button
             type="button"
             class="block w-full py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-          @click="borrarCita(citaItem.email)"
-
+            @click="borrarCita(citaItem.id)"
           >
             Eliminar
           </button>
