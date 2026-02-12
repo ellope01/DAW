6ebabe7651b4c9ae5c4eac9bab0c45b9efde15aa
ellope01 <algieba23@gmail.com>
2026-02-12 23:54:16 +0100

    pagina usuario

--- merliah/resources/js/Pages/usuario.vue
+++ merliah/resources/js/Pages/usuario.vue
@@ -6,20 +6,20 @@
 
         <div class="h-4/5 flex items-center justify-center">
             <div
-                class="bg-[#FF9FD4]/65 h-9/10 flex items-center justify-center w-9/10 rounded-[2rem] gap-3"
+                class="bg-[#FF9FD4]/65 h-9/10 flex items-center justify-center w-9/10 rounded-[2rem] gap-7"
             >
-                <div class="bg-neutral-300 h-4/5 w-4/10 rounded-2xl">
+                <div class="bg-neutral-300 h-4/5 w-5/10 rounded-2xl">
                     Imagen
                 </div>
-                <div class="flex flex-col">
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Información usuario</p>
+                <div class="flex flex-col w-4/10 gap-3">
+                    <div class="bg-[#FFBB4E]/70 w-full h-30 rounded-4xl flex items-center justify-center">
+                        <p class="text-5xl">Información usuario</p>
                     </div>
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Pedidos</p>
+                    <div class="bg-[#FFBB4E]/70 w-full h-30 rounded-4xl flex items-center justify-center">
+                        <p class="text-5xl">Pedidos</p>
                     </div>
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Favoritos</p>
+                    <div class="bg-[#FFBB4E]/70 w-full h-30 rounded-4xl flex items-center justify-center">
+                        <p class="text-5xl">Favoritos</p>
                     </div>
                 </div>
             </div>
