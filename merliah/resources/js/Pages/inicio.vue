b5b372f127ab6e86605cee46f2ee7ca23e696a56
ellope01 <algieba23@gmail.com>
2026-04-21 07:14:03 +0200

    commit cambio de ordenador

--- merliah/resources/js/Pages/inicio.vue
+++ merliah/resources/js/Pages/inicio.vue
@@ -4,24 +4,13 @@
             Header
         </div>
 
-        <div class="h-4/5 flex items-center justify-center">
-            <div
-                class="bg-[#FF9FD4]/65 h-9/10 flex items-center justify-center w-9/10 rounded-[2rem] gap-3"
-            >
-                <div class="bg-neutral-300 h-4/5 w-4/10 rounded-2xl">
-                    Imagen
-                </div>
-                <div class="flex flex-col">
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Información usuario</p>
-                    </div>
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Pedidos</p>
-                    </div>
-                    <div class="bg-neutral-300 w-4/5 h-6">
-                        <p>Favoritos</p>
-                    </div>
-                </div>
+        <div class="h-4/5 flex flex-col">
+            <!--Poner un carrusel o algo de cosas-->
+            <div class="bg-neutral-300 h-full w-full" >
+                <p>Carrusel de cosas</p>
+            </div>
+            <div >
+                <h2>Best sellers</h2>
             </div>
         </div>
 
